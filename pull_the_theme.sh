68f9a3dd25ea64179ab9e726f62f0d2db214dc80
cai_fedora <2930416610@qq.com>
2026-05-03 17:33:01 +0800

    deletet the tar verbose

--- pull_the_theme.sh
+++ pull_the_theme.sh
@@ -3,6 +3,6 @@
 mkdir -p themes
 wget https://dong-dynabook-satellite-b35-r.tail015922.ts.net/static/file_manager/data/Share/static/hugo-theme-stack.tar.xz
 
-tar xJvf hugo-theme-stack.tar.xz -C themes/
+tar xJf hugo-theme-stack.tar.xz -C themes/
 
 rm hugo-theme-stack.tar.xz
