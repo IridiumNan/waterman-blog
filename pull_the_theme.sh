04e730c6e13eaa370b07ea3cad2c69d487752d79
cai_fedora <16326494+cai-zixiang_hainan@user.noreply.gitee.com>
2026-05-26 09:39:53 +0800

    add the principles for tailscale and modify the tailscale analysis

--- pull_the_theme.sh
+++ pull_the_theme.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-mkdir -p themes
-wget https://dong-dynabook-satellite-b35-r.tail015922.ts.net/static/file_manager/data/Share/static/hugo-theme-stack.tar.xz
+SRC='https://dong-dynabook-satellite-b35-r.tail015922.ts.net/static/file_manager/data/Share/static/hugo-theme-stack.tar.xz'
 
+mkdir -p themes
+wget $SRC
+
+echo "unpacking the stack theme"
 tar xJf hugo-theme-stack.tar.xz -C themes/
 
 rm hugo-theme-stack.tar.xz
