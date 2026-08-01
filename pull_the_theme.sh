e121d50b6a94e901058e78dfd9df510ebbb87d36
cai_fedora <2930416610@qq.com>
2026-08-01 22:04:01 +0800

    Add the theme and update friend link

--- pull_the_theme.sh
+++ pull_the_theme.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-SRC='https://rocky-colorful.tail015922.ts.net/wt/public/hugo-theme-stack.tar.xz'
+#SRC='https://rocky-colorful.tail015922.ts.net/wt/public/hugo-theme-stack.tar.xz'
 
-mkdir -p themes
-wget $SRC
+#mkdir -p themes
+#wget $SRC
 
-echo "unpacking the stack theme"
+#echo "unpacking the stack theme"
 tar xJf hugo-theme-stack.tar.xz -C themes/
 
 rm hugo-theme-stack.tar.xz
